81e56de2bc475753a5a94f03d37ccd36b929c7d6
mar-hein <mar.hein@hotmail.com>
2023-08-05 20:18:10 +0200

    Changed navbar to functional component

--- src/main/js/components/Navbar/Navbar.js
+++ src/main/js/components/Navbar/Navbar.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { NavbarWrapper } from './Navbar.styled';
+const client = require('./../../client');
 
-const Navbar = () => (
- <NavbarWrapper>
-    Navbar Component
- </NavbarWrapper>
-);
+function Navbar() {
 
-Navbar.propTypes = {};
+    const [tabs, setTabs] = useState([]);
 
-Navbar.defaultProps = {};
+    useEffect(() => {
+            client({method: 'GET', path: '/api/tabs'}).then(response => {
+                setTabs(response.entity._embedded.tabs)});
+            });
+
+    return (
+         <NavbarWrapper>
+            <h1>++++</h1>
+         </NavbarWrapper>
+    );
+};
 
 export default Navbar;
--- src/main/js/components/Navbar/Navbar.styled.js
+++ src/main/js/components/Navbar/Navbar.styled.js
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
 
 export const NavbarWrapper = styled.div`
+    display: flex;
+    width: 90%;
+    flex-direction: row;
+    margin-top: 1rem;
+    flex-wrap: wrap;
 `;
